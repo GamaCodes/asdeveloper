8c94fd9d8482cba75e250ac13931ecd7fb90abc4
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2020-03-11 02:07:00 -0600

    Routes created

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/home/Home';
-import NotFound from './components/404/NotFound.js';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Home from './pages/home';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+import NotFound from './pages/notFound';
 
 const Router = () => (
   <BrowserRouter>
+    <Navbar/>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
       <Route component={NotFound} />
     </Switch>
+    <Footer/>
   </BrowserRouter>
 );
 
---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+function Home() {
+  return (
+    <Box bgcolor="#353F49" width="100vw" height="8vh">
+      <Button variant="contained" color="primary">
+        Hola Mundo! Pruebas de app
+      </Button>
+    </Box>
+  );
+}
+
+export default Home;
